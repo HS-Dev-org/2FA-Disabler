adc5db4e0505f1c46e24f4a03db044c148c3c20d
Stanley <66573599+Stanley-GF@users.noreply.github.com>
2021-02-01 18:17:51 +0100

    Add files via upload

---
+++ src/Auto2FADisabler/Program.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Auto2FADisabler
+{
+    public class DiscordThings
+    {
+        [JsonProperty("code")]
+        public string code { get; set; }
+
+    }
+
+    class Program
+    {
+        public static string Get2FACode(string Token, string password, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v8/users/@me/mfa/codes");
+
+            var postData = Properties.Text.password.Replace("my1337pass", password);
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "POST";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+
+
+
+        public static string Disable2FA(string Token, string code, string UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36")
+        {
+            var Req = (HttpWebRequest)WebRequest.Create($"https://discord.com/api/v8/users/@me/mfa/totp/disable");
+
+            var postData = Properties.Text.DisableMFA.Replace("urcode", code);
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            Req.Method = "POST";
+            Req.UserAgent = UserAgent;
+            Req.ContentType = "application/json";
+            Req.Headers.Add("authorization", Token);
+            Req.ContentLength = data.Length;
+
+            using (var stream = Req.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)Req.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Coded by https://github.com/HS-Dev-org/ organisation, have fun :)");
+            Thread.Sleep(2500);
+            Console.Clear();
+            Console.ResetColor();
+            Console.Write("Token : "); string token = Console.ReadLine(); Console.Clear(); Console.Write("Password : "); string password = Console.ReadLine();
+
+
+
+            try
+            {
+                string _2faasjson = Get2FACode(token, password);
+                dynamic deserialized = JObject.Parse(_2faasjson);
+
+
+                string backupcode1 = deserialized.backup_codes[0].code;
+
+
+                string mfadisablereturned = Disable2FA(token, backupcode1);
+
+
+                dynamic newtoken = JObject.Parse(mfadisablereturned);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Succesfully disabled 2FA : " + newtoken.token);
+            }
+            catch (Exception)
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("An error occured");
+            }
+
+            
+            Thread.Sleep(-1);
+        }
+    }
+}
---
+++ src/Auto2FADisabler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Auto2FADisabler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Auto2FADisabler")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("ada7aa11-7475-4465-8770-9c58f30d6185")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
